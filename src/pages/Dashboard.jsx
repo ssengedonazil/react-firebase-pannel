eb0772f699b9126bd544e79a15ae8b1f29e87a0c
Ssengendo Naz Hill (POM) <nssegendo@projectcode.ug>
2025-07-30 13:48:38 +0300

    card

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,72 +1,24 @@
 import React, { useState, useEffect } from "react";
-// import { collection, getDocs, addDoc, deleteDoc, setDoc, doc, } from "firebase/firestore";
-// import { db, auth } from "../firebase";
 import UserTable from "../components/UserTable";
 import AddClientsModal from "../components/AddClientsModal";
 import AddUsersModal from "../components/AddUsersModal";
 import {  closeModel,  tableClients,  tableAuth,  handleAddUser,  handleAddClient,  handleDeleteUser,  fetchUsers,} from "../components/functions";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-// import DataBaseTable from "../Constants/Tables";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"; 
 
 
 
-export default function Dashboard() {
-  
-  // const tableClients=DataBaseTable.clients
-  // const tableAuth=DataBaseTable.users
+export default function Dashboard() { 
   const [users, setClients] = useState([]);
   const [modalState, setModalState] = useState({ show: false, title: null });
 
-  // function closeModel() {
-  //   setModalState({ show: false, title: null });
-  // }
+ 
   useEffect(() => {
     fetchUsers(setClients);
   }, []);
 
-  // const fetchUsers = async () => {
-  //   const snapshot = await getDocs(collection(db, tableClients));
-  //   const userList = snapshot.docs.map((doc) => ({
-  //     id: doc.id,
-  //     ...doc.data(),
-  //   }));
-  //   setClients(userList);
-  // };
+   ;
 
-  // const handleAddUser = async (formData) => {
-  //   const { Name, Email, Password } = formData;
-  //   if (Name.trim() && Email.trim() && Password.trim()) {
-  //     try {
-  //       const userCredential = await createUserWithEmailAndPassword(
-  //         auth,
-  //         Email,
-  //         Password,
-  //         Name
-  //       );
-  //       const user = userCredential.user;
-  //       await setDoc(doc(db, "users", user.uid), {
-  //         Name,
-  //         Email,
-  //         createdAt: new Date(),
-  //       });
 
-  //       closeModel();
-  //     } catch (error) {
-  //       console.error("Error creating user:", error.message);
-  //       alert(error.message); // or show a Toast
-  //     }
-  //   }
-  // };
-
-  // const handleAddClient = async (client) => {
-  //   const docReferance = await addDoc(collection(db, tableClients), client);
-  //   setClients((prev) => [...prev, { id: docReferance.id, ...client,createdAt: new Date(), }]);
-  // };
-
-  // const handleDeleteUser = async (id) => {
-  //   await deleteDoc(doc(db, "Users", id));
-  //   setClients(users.filter((u) => u.id !== id));
-  // };
 
   return (
     <div className="container mt-5">
